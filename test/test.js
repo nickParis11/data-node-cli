b29d7bf342473be7afd1cf4390c18e9f5efe2b08
Niklas <norr1000@gmail.com>
2018-03-27 20:46:22 +0200

    add test for 10C00 moves

--- test/test.js
+++ test/test.js
@@ -9,22 +9,12 @@ const assert = require('assert');
 const { expect } = require("chai");
 var stdout = require("test-console").stdout;
 
-var robot;
-var ETL;
+var robot = robot = require('./../robot');
+var ETL = ETL = require('./../ETL');
 
 
-beforeEach(function() {
-    // runs before each test in this block
-			robot = require('./../robot');
-			ETL = require('./../ETL');
-  });
-
-afterEach(function() {
-    robot = '';
-    ETL ='';
-
-  });
-
+log('testing ....')
+log('\n\n****  please wait, it may take a while to compute all possibilities  ****')
 
 // ************ processing tests *******************
 
@@ -424,6 +414,33 @@ describe('Robot algorithm - no or fake moves', function() {
     expect(output).to.deep.equal([ '=> cleaned: 1\n' ])
   });
 
-
 });
 
+describe('Robot algorithm - 10 000 moves', function() {
+
+ const letters = ['N','S','E','W'];
+
+ const moves = [];
+
+ const length = 10000;
+
+ function getMoveNum () {
+   return Math.floor((Math.random()*200001)-100000);
+ }
+
+ for ( var i = length; i > 0; i -= 1) {
+  const index = Math.floor(Math.random()*letters.length);
+  const num = getMoveNum();
+  const move = letters[index]+ ' ' + num.toString();
+  moves.push(move);
+ }
+
+ var output =  stdout.inspectSync(function() {
+   ETL.trigger( moves,robot.getCleanedPiecesNum)
+ });
+
+  it("Should correctly count a lot of moves", () => {
+    expect(output).to.not.be.undefined;
+ });
+
+});
